f16b4a40f2dea05530c0e7a6b285b47eb0dac210
George Mack <george.mack84@gmail.com>
2026-04-12 22:53:12 -0400

    Suppress OpenTelemetry webpack warning

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings ?? []),
+      {
+        module: /@opentelemetry[\\/]instrumentation/,
+        message: /Critical dependency: the request of a dependency is an expression/,
+      },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
