02bffd174ea72539d97628f39d74206999ce020a
George Mack <george.mack84@gmail.com>
2026-09-09 02:57:56 -0400

    fix: align outbox database factory lifetime

--- services/api/Proprium.Infrastructure/ServiceCollectionExtensions.cs
+++ services/api/Proprium.Infrastructure/ServiceCollectionExtensions.cs
@@ -25,7 +25,6 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPropriumInfrastructure(this IServiceCollection services)
     {
-        services.AddDbContext<PropriumDbContext>((provider, options) => options.UseNpgsql(provider.GetRequiredService<IOptions<PostgresOptions>>().Value.BuildConnectionString()));
         services.AddDbContextFactory<PropriumDbContext>((provider, options) => options.UseNpgsql(provider.GetRequiredService<IOptions<PostgresOptions>>().Value.BuildConnectionString()));
         services.AddSingleton<IConnectionMultiplexer>(provider =>
         {
